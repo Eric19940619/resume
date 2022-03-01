5eeb7a508b59e04bc6fa0b246365f630b8422841
yu8306191 <yu8306191@gmail.com>
2022-03-01 14:02:29 +0800

    fix react-i18next

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,24 +1,29 @@
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import en from '../local/language/en.json'
-import tw from '../local/language/zh-TW.json'
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+import en from "../local/language/en.json";
+import tw from "../local/language/zh-TW.json";
 
 const resources = {
   en: {
-    translation: en
+    translation: en,
   },
-  'zh-TW': {
-    translation: tw
-  }
-}
+  "zh-TW": {
+    translation: tw,
+  },
+};
 
-i18n.use(initReactI18next).init({
-  fallbackLng: 'en',
-  lng: 'zh-TW',
-  resources,
-  interpolation: {
-    escapeValue: false
-  }
-})
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    resources,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
-export default i18n
+export default i18n;
